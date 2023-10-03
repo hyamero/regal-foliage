c8e6a8838a24683a0a90329dfd36a9966ac17ffc
Dale B <daleban.dev@gmail.com>
2023-10-03 11:05:43 +0800

    chore: hero section design

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,23 @@ function App() {
           src={landingImage}
           alt="Landing Background Image"
         />
-        <h1 className="p-20 text-white text-9xl font-neue-light">
-          Regal Foliage
-        </h1>
+        <div className="relative top-32 left-32 space-y-10 inline-block gap-10">
+          <h1 className="text-white text-9xl font-neue-light">Regal Foliage</h1>
+
+          <div className="w-full h-[1px] bg-white" />
+
+          <div className="gap-10 flex font-neue-light text-white items-center justify-between">
+            <p className=" text-3xl">
+              Transforming spaces with nature's opulence.
+            </p>
+            <button
+              type="button"
+              className="inline py-2 whitespace-nowrap px-6 text-2xl border-[2px] rounded-full"
+            >
+              Shop now
+            </button>
+          </div>
+        </div>
       </section>
     </>
   );
