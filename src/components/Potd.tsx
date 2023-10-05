fd306b634ba57e3b61f67716fc4a93d947a4c0d5
Dale B <daleban.dev@gmail.com>
2023-10-05 17:28:11 +0800

    chore: plant of the day section responsiveness

--- src/components/Potd.tsx
+++ src/components/Potd.tsx
@@ -1,20 +1,23 @@
 import { Container } from "./Container";
 import potd from "../assets/images/potd.png";
+import { IoCartOutline } from "react-icons/io5";
 
 export const Potd = () => {
   return (
     <section className="min-h-screen bg-secondary text-primary border-b-[2px] border-primary">
       <Container className="pt-56">
-        <h2 className="text-[11rem] tracking-tight font-neue-roman leading-none">
+        <h2 className="text-9xl md:text-[9.5rem] lg:text-[11rem] tracking-tight font-neue-roman leading-none">
           POTD
         </h2>
-        <p className="font-neue-light text-3xl">( plant of the day )</p>
+        <p className="font-neue-light text-3xl ml-1">( plant of the day )</p>
 
-        <div className="flex justify-between items-end gap-36">
-          <div className="flex flex-col gap-12">
+        <div className="flex justify-between lg:flex-row flex-col items-end gap-0 lg:gap-36">
+          <div className="flex flex-col gap-12 mt-36 xl:mt-0">
             <div>
               <div className="mb-5 flex gap-3 font-neue-light items-center justify-between">
-                <h3 className="text-8xl font-neue-light">regal bonsai</h3>
+                <h3 className="2xl:text-8xl sm:text-7xl text-6xl lg:text-7xl font-neue-light">
+                  regal bonsai
+                </h3>
                 <p className="py-1 whitespace-nowrap font-neue-roman px-6 text-xl border-[2px] rounded-full border-primary">
                   $269
                 </p>
@@ -29,26 +32,27 @@ export const Potd = () => {
 
             <div className="w-full h-[1px] bg-primary" />
 
-            <div className="flex justify-between pb-44 font-neue-roman">
-              <div className="flex gap-5">
-                <p className="py-2 whitespace-nowrap px-6 text-2xl border-[2px] border-primary rounded-full">
-                  bonsai
-                </p>
-                <p className="py-2 whitespace-nowrap px-6 text-2xl border-[2px] border-primary rounded-full">
-                  regal
-                </p>
+            <div className="flex justify-between sm:flex-row flex-col gap-5 pb-44 font-neue-roman">
+              <div className="flex sm:gap-5 gap-3 [&>p]:py-2 [&>p]:whitespace-nowrap [&>p]:px-6 [&>p]:sm:text-2xl text-lg [&>p]:border-[2px] [&>p]:border-primary [&>p]:rounded-full">
+                <p>bonsai</p>
+                <p>regal</p>
               </div>
 
               <button
-                className="py-2 whitespace-nowrap px-6 text-2xl border-[2px] rounded-full bg-primary text-white"
+                className="py-2 flex gap-2 items-center whitespace-nowrap px-6 sm:text-2xl text-lg border-[2px] rounded-full bg-primary text-white justify-center"
                 type="button"
               >
-                Add to Cart
+                <IoCartOutline className="text-3xl" />
+                <p>Add to Cart</p>
               </button>
             </div>
           </div>
 
-          <img src={potd} alt="Regal Bonsai: Plant of the day" />
+          <img
+            src={potd}
+            alt="Regal Bonsai: Plant of the day"
+            className="mx-auto"
+          />
         </div>
       </Container>
     </section>
