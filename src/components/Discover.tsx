678e8bed354d2a414871e908a229110433010bf9
Dale B <daleban.dev@gmail.com>
2023-10-05 14:30:15 +0800

    chore: discover section design layout

--- src/components/Discover.tsx
+++ src/components/Discover.tsx
@@ -1,5 +1,11 @@
 import { Container } from "./Container";
 import { plant1, plant2, plant3 } from "../assets/images";
+import {
+  PiArrowUpRightLight,
+  PiArrowRightLight,
+  PiArrowLeftLight,
+} from "react-icons/pi";
+// import { SlArrowRightCircle, SlArrowLeftCircle } from "react-icons/sl";
 
 export const Discover = () => {
   const plants = [
@@ -20,50 +26,71 @@ export const Discover = () => {
     },
   ];
 
+  const plantsTag = [
+    "Bonsai",
+    "Jade",
+    "Saffron",
+    "Obliqua",
+    "Bonsai",
+    "Jade",
+    "Saffron",
+    "Obliqua",
+  ];
+
   return (
-    <section className="bg-secondary min-h-screen">
+    <section className="font-neue-roman flex flex-col gap-20 py-56 bg-secondary min-h-screen">
       <Container>
         <h2 className="text-primary text-[11rem] tracking-tight font-neue-roman leading-none">
-          discover
+          discover.
         </h2>
+      </Container>
 
-        <div className="w-full h-[1px] bg-white" />
+      <div className="w-full h-[1px] bg-[#B1B1B1]" />
 
-        <div>
-          <div>
-            <h3>Transforming spaces with nature's opulence.</h3>
+      <Container className="grid grid-cols-3 gap-8">
+        <div className="flex flex-col gap-10">
+          <h3 className="text-3xl">
+            Transforming spaces with nature's opulence.
+          </h3>
 
-            <ul>
-              <li>plant</li>
-              <li>plant</li>
-              <li>plant</li>
-              <li>plant</li>
-              <li>plant</li>
-              <li>plant</li>
-              <li>plant</li>
-              <li>plant</li>
-            </ul>
-
-            <button>arrow</button>
-            <button>arrow</button>
-          </div>
-
-          <div>
-            {plants.map(({ name, image, price }) => (
-              <div key={name}>
-                <img src={image} alt={name} />
-                <div>
-                  <div>
-                    <h4></h4>
-                    <p>arrow icon</p>
-                  </div>
-                  <p className="py-1 whitespace-nowrap font-neue-roman px-6 text-xl border-[2px] rounded-full border-primary">
-                    ${price}
-                  </p>
-                </div>
-              </div>
+          <ul className="flex flex-col gap-2">
+            {plantsTag.map((plant) => (
+              <li key={plant} className="cursor-pointer">
+                {plant}
+              </li>
             ))}
+          </ul>
+
+          <div className="flex gap-5 text-3xl [&>*]:border [&>*]:p-2 [&>*]:rounded-full [&>*]:text-5xl [&>*]:border-black [&>*]:cursor-pointer">
+            <PiArrowLeftLight />
+            <PiArrowRightLight />
           </div>
+        </div>
+
+        <div className="flex gap-10 col-span-2">
+          {plants.map(({ name, image, price }) => (
+            <div
+              key={name}
+              className="bg-white p-3 gap-4 rounded-sm flex flex-col"
+            >
+              <div className="h-full w-full overflow-hidden">
+                <img
+                  src={image}
+                  alt={name}
+                  className="cursor-pointer hover:scale-110 transition-transform duration-300 ease-in-out  object-cover object-center h-full w-full"
+                />
+              </div>
+              <div className="flex justify-between">
+                <div className="text-xl flex gap-1 cursor-pointer  items-center">
+                  <h4>{name}</h4>
+                  <PiArrowUpRightLight />
+                </div>
+                <p className="whitespace-nowrap px-4 text-base border-[1.5px] rounded-full border-primary text-primary">
+                  ${price}
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
       </Container>
     </section>
